62113028409ca36807b25f5e039273d4ab69b6bc
zhuliu4 <328260148@qq.com>
2022-06-16 18:12:34 +0800

    增加celery定时任务策略

--- mycelery/main.py
+++ mycelery/main.py
@@ -3,13 +3,14 @@ from celery import Celery
 from django.conf import settings
 
 # 设置celery环境变量和django-celery的工作目录
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CeleryTask.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
 
 # 实例化celery
-app = Celery("our_projeect")
+cel = Celery("tutorial")
 
 # 加载celery配置
-app.config_from_object("django.conf:settings")
+# cel.config_from_object("django.conf:settings")
+cel.config_from_object("config")
 
 # 如果项目当中有task.py, 那么celery使用app当中的task来生成任务
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+cel.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
